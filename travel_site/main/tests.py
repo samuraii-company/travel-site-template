81d4fe132c4673edc9dffb1463ab77c18b9ee773
samuraii <83868709+samuraii-company@users.noreply.github.com>
2021-11-22 11:43:00 +0300

    css fix

--- travel_site/main/tests.py
+++ travel_site/main/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import UserData, Subscirbe_Emails, Destinations, Blog
 # Create your tests here.
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.userdata = UserData.objects.create(
+            name="test name",
+            phone_number="+7922222222",
+            email="test@gmail.com")
+
+    def test_post_model(self):
+        d = self.userdata
+        self.assertTrue(isinstance(d, UserData))
+        self.assertEqual(str(d), "test name")
